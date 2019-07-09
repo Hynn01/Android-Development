1df0197890dc03a002974a4a34e06443a6f080a8
Hynn01 <hynn0633@163.com>
2019-07-09 21:23:14 +0800

    帮助界面ok

--- DBDemo/app/src/main/java/com/example/dbdemo/activity/helpActivity.java
+++ DBDemo/app/src/main/java/com/example/dbdemo/activity/helpActivity.java
@@ -1,17 +1,50 @@
 package com.example.dbdemo.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.dbdemo.R;
+import com.youth.banner.Banner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class helpActivity extends Activity {
+
+    private ImageButton bt_back;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help);
+
+        bt_back=(ImageButton)findViewById(R.id.bt_back);
+
+        Banner banner = (Banner) findViewById(R.id.banner);
+        //本地图片数据（资源文件）
+        List<Integer> list=new ArrayList<>();
+        list.add(R.mipmap.ad1);
+        list.add(R.mipmap.ad2);
+        list.add(R.mipmap.ad3);
+        banner.setImages(list)
+                .setImageLoader(new GlideImageLoader())
+                .start();
+
+        //（14）为返回按钮添加监听
+        //（15）在当按下返回按钮时，调用返回到主菜单界面函数返回
+        bt_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(helpActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
