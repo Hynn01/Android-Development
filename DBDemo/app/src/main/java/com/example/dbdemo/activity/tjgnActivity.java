6696a90a012ba5d8e7ff416b81b5f80ce5daa7cb
Hynn01 <hynn0633@163.com>
2019-07-09 21:10:28 +0800

    站次添加

--- DBDemo/app/src/main/java/com/example/dbdemo/activity/tjgnActivity.java
+++ DBDemo/app/src/main/java/com/example/dbdemo/activity/tjgnActivity.java
@@ -8,6 +8,10 @@ import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.example.dbdemo.R;
+import com.youth.banner.Banner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //附加功能
 public class tjgnActivity extends Activity {
@@ -16,12 +20,25 @@ public class tjgnActivity extends Activity {
     private Button bt_tjcz;//添加车站
     private Button bt_tjcc;//添加车次
 
+    private ImageButton bt_back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         //（1）添加功能 goTofjgnView()
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tjgn);
+        bt_back=(ImageButton)findViewById(R.id.back_zctj);
+
+        Banner banner = (Banner) findViewById(R.id.banner_tjgn);
+        //本地图片数据（资源文件）
+        List<Integer> list=new ArrayList<>();
+        list.add(R.mipmap.ad1);
+        list.add(R.mipmap.ad2);
+        list.add(R.mipmap.ad3);
+        banner.setImages(list)
+                .setImageLoader(new GlideImageLoader())
+                .start();
 
         //（2）关系添加按钮引用
         bt_tjgx=(Button)findViewById(R.id.bt_tjgx);
@@ -58,6 +75,15 @@ public class tjgnActivity extends Activity {
             }
         });
 
+        //（26）为返回按钮添加监听
+        //（27）返回主菜单界面
+        bt_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(tjgnActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
