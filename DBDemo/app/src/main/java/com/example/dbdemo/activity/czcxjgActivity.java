a39d3d9ceb28bbe7a0928ba1f89f3864336a5aad
Hynn01 <hynn0633@163.com>
2019-07-09 17:16:00 +0800

    加上了结果页面

--- DBDemo/app/src/main/java/com/example/dbdemo/activity/czcxjgActivity.java
+++ DBDemo/app/src/main/java/com/example/dbdemo/activity/czcxjgActivity.java
@@ -1,17 +1,17 @@
 package com.example.dbdemo.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.dbdemo.R;
 
 //站站查询结果
-public class zzcxjgActivity extends AppCompatActivity {
+public class czcxjgActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.zzcxjg);
+        setContentView(R.layout.czcxjg);
         //todo:放一个listview
     }
 }
