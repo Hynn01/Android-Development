357fc6659d35f2ffc03022f41baff5489c527e57
Hynn01 <hynn0633@163.com>
2019-07-09 08:31:33 +0800

    update

--- DBDemo/app/src/main/java/com/example/dbdemo/activity/MainActivity.java
+++ DBDemo/app/src/main/java/com/example/dbdemo/activity/MainActivity.java
@@ -1,4 +1,67 @@
 package com.example.dbdemo.activity;
 
-public class MainActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.example.dbdemo.R;
+
+public class MainActivity extends Activity {
+
+    private ImageButton bt_about;
+    private ImageButton bt_help;
+    private ImageButton bt_zzcx;
+    private ImageButton bt_cccx;
+    private ImageButton bt_czcx;
+    private ImageButton bt_fjgn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //（1）一开始进入莱单界面方法就是goToMainMenu()函数
+        //（2）切换到莱单界面 使用setContentView（）
+        //（3）标识当前界面为菜单界面
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        //（4）获取菜单界面下的各个按钮的引用（包括，关于help，站站查询，车次查询，车站查询，添加功能的按钮）
+        bt_about=(Button)findViewById(R.id.bt_about);
+
+        //以下为各个按钮监听的处理的实现
+        //（5）为关干按钮添加监听 关于按钮的监听 发消息进入关于界面，参考上面send MSG的方法
+        //给bt1添加点击事件
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                第一个参数:上下文对象this
+                第二个参数:目标文件
+                 */
+                Intent intent = new Intent(MainActivity.this,aboutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //（6）帮助查询的监听 发消息进入帮助界面
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                第一个参数:上下文对象this
+                第二个参数:目标文件
+                 */
+                Intent intent = new Intent(MainActivity.this,aboutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //（7）站站查询按钮的监听 进入站站查询模块
+        //（8）车次查询按钮的监听 进入车次查询模块
+        //（9）车站所有车次查询 进入车站查询模块
+        //（10）附加功能按钮的监听 进入附加功能模块
+
+    }
+
 }
--- DBDemo/app/src/main/java/com/example/dbdemo/activity/aboutActivity.java
+++ DBDemo/app/src/main/java/com/example/dbdemo/activity/aboutActivity.java
@@ -1,4 +1,19 @@
 package com.example.dbdemo.activity;
 
-public class aboutActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.dbdemo.R;
+
+public class aboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //（1）一开始进入莱单界面方法就是goToMainMenu()函数
+        //（2）切换到莱单界面 使用setContentView（）
+        //（3）标识当前界面为菜单界面
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
 }
